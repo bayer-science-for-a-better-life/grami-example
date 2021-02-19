71a364ea36d7688504f8afe4521357dd0c4144a0
hkuich <henning.kuich@bayer.com>
2021-02-19 10:54:03 +0100

    - updated to work with grami-0.1.0-alpha-9

--- src/main/java/migration/Migration.java
+++ src/main/java/migration/Migration.java
@@ -13,14 +13,14 @@ public class Migration {
     private static final String dataConfig = getAbsPath("src/main/resources/dataConfig.json");
     private static final String migrationStatus = getAbsPath("src/main/resources/migrationStatus.json");
 
-    private static final String graknURI = "127.0.0.1:48555";
+    private static final String graknURI = "127.0.0.1:1729";
     private static final String keyspaceName = "grami_example";
 
     private static final MigrationConfig migrationConfig = new MigrationConfig(graknURI, keyspaceName, schema, dataConfig, processorConfig);
 
     public static void main(String[] args) throws IOException {
         GraknMigrator mig = new GraknMigrator(migrationConfig, migrationStatus, true);
-        mig.migrate(true, true);
+        mig.migrate(true, true, true, true);
     }
 
 }
